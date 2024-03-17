44ed3dfaf0a8aeb42922baa50e44d6c3c61ee324
Doclikam <likamwambui28@gmail.com>
2024-03-17 13:51:29 -0700

    messages

--- main copy.py
+++ main copy.py
@@ -1,14 +1,20 @@
-             
 import datetime as dt 
 import os.path
-import pickle
+
+import schedule
+from datetime import datetime, timedelta
+
+
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import requests
-from flask import Flask
+
+
+import schedule
 from datetime import datetime, timedelta
 
 from twilio.rest import Client
@@ -44,82 +50,82 @@ def main():
             #allowing the user to sign into their accounts
             flow=InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds=flow.run_local_server(port=0)
+        
 
             
 
         with open('token.json','w') as token:
                 token.write(creds.to_json())
 
-def build():
+
+
+    try:
         #building a service object
         service=build('calendar', 'v3', credentials=creds)
+        
 
-   
+        
+    
+        #get the time the function is called timeMin
+        now = dt.datetime.utcnow().isoformat() + 'Z'
+        print(now)
+        
 
 
-def now():
-    #get the value timeMin and timeMax to the current date
-    print(datetime.date.day)
-
-    #get the time the function is called timeMin
-    now = dt.datetime.utcnow().isoformat() + 'Z'
-    print(now)
-    return now
-
-try: 
-    def tommorow():
         #get the the time 24 hours from now
         tomorrow = (dt.datetime.utcnow() + timedelta(days=1)).isoformat() + 'Z'
         print(tomorrow)
-        return tommorow
+        
 
 
     
-    #get all the vent scheduled within 24 hours
-    events_result = service.events().list(
+        #get all the vent scheduled within 24 hours
+        events_result = service.events().list(
             calendarId='primary',
             timeMin=now ,
             timeMax = tomorrow,
             singleEvents=True,
             orderBy='startTime').execute()
 
-    events = events_result.get('items', [])
-    event_plans=[]
-    if events:
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            #print(start, event['summary'])
-            event_plans.append(start)
+        events = events_result.get('items', [])
+        event_plans=[]
+        if events:
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                #print(start, event['summary'])
+                event_plans.append(start)
 
 
-            msg= Client.messages.create(
+                msg= Client.messages.create(
                     to=user_num,
                     from_=twilio_num,
                     body=event_plans + event['summary'])
+                print(msg.sid)
+    
+
+        else:
+            msg= client.messages.create(
+                    to=user_num,
+                    from_=twilio_num,
+                    body='You have no scheduled events in your calendar for tommorow')
             print(msg.sid)
+
+        #automating the messages at a certain time everyday
+        schedules=schedule.every().day.at('12:30').do(send_schedule)
     
 
-    else:
-
-        
-        print('No events in your calendar')
-
-
-    #automating the messages at a certain time everyday
-    
-    schedules=schedule.every().day.at('12:30').do(send_schedule)
-        
-
-
-except HttpError as error:
+    except HttpError as error:
         print(f'An error occured!: {error}')
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
 
 
-    
-
-        
 
     
